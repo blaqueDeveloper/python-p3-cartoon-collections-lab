31b03844fe2d632460ffb2dd941d85d0db03c16e
Blaque <blaque.develop@gmail.com>
2023-08-23 12:11:23 +0300

    Done with assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,26 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+  index = 1
+  for name in names:
+    print(f'{index}. {name}')
+    index += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+  newCall = [call.title() + "!" for call in planeteer_calls]
+  return newCall
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(call):
+  for name in call:
+    if len(name) > 4:
+      return True
+  return False  
 
-def find_the_cheese():
-    pass
+
+
+def find_the_cheese(meals):
+  cheese = ['cheddar', 'gouda', 'camembert']
+  for name in cheese:
+    if name in meals:
+      return name
+    else:
+      return None
+
